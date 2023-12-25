ce3bf692086819d5a68c4cb7b6bc72b727cc3890
Shin Welivitiya <shinvitiya@gmail.com>
2023-12-25 18:13:55 +0530

    initial commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            height={38} 
+            width={128}
+            alt="logo"
+          />
+        </Link>
+
+        <p>2023 Evently. All Rights Reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
